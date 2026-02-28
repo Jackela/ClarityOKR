0c803764ee346f413d006ff9add51a3d30cf50b7
Jackela <k740724287@gmail.com>
2026-02-28 11:20:57 +0800

    fix(test): use jsdom test environment for integration tests

--- tests/integration/jest.config.cjs
+++ tests/integration/jest.config.cjs
@@ -2,15 +2,14 @@ const { defaultsESM } = require('ts-jest/presets');
 
 module.exports = {
   ...defaultsESM,
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/specs'],
   extensionsToTreatAsEsm: ['.ts'],
   setupFilesAfterEnv: ['<rootDir>/setup.ts'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '../../tsconfig.base.json', useESM: true }]
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '../../tsconfig.base.json', useESM: true }],
   },
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  }
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
-
