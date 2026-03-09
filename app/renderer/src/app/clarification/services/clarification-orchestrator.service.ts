4c041c248913135a3c82f4cdb7b9c18c7b029117
Jackela <k740724287@gmail.com>
2026-03-09 10:50:24 +0800

    FIX: Add import/order eslint-disable to orchestrator service

--- app/renderer/src/app/clarification/services/clarification-orchestrator.service.ts
+++ app/renderer/src/app/clarification/services/clarification-orchestrator.service.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-redundant-type-constituents */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-redundant-type-constituents, import/order */
 import { Injectable, NgZone } from '@angular/core';
 import {
   clarificationOptionSelectionSchema,
