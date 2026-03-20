c3476f24bd5a40f463b08743496b91281849331c
Jackela <k740724287@gmail.com>
2026-03-20 13:32:04 +0800

    fix: remove greedy .js stripping pattern that was breaking module resolution

--- tests/integration/jest.config.cjs
+++ tests/integration/jest.config.cjs
@@ -12,7 +12,6 @@ module.exports = {
   },
   moduleNameMapper: {
     '^@clarityokr/(.*)$': '<rootDir>/../../packages/$1/dist/index.js',
-    '^(\\.{1,3}/.*)\\.js$': '<rootDir>/../../$1',
     '^../../../app/main/src/services/(.*)\\.js$': '<rootDir>/../../app/main/src/services/$1',
     '^../../../app/main/(.*)$': '<rootDir>/../../app/main/$1',
     '^electron$': '<rootDir>/__mocks__/electron.ts',
